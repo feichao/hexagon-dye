f971fcb2c72ed4885f59e66b9e98ed8a185c122d
feichao <len.may@foxmail.com>
2016-12-12 19:46:22 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+var hexWrap = document.getElementById('hex-wrap');
+var hexDye = document.getElementById('hex-dye');
+
+function getLeft() {
+  return hexWrap.offsetWidth / 2 * Math.random();
+}
+
+function getTop() {
+  return hexWrap.offsetHeight / 2 * Math.random();
+}
+
+function getWidth() {
+  return (hexWrap.offsetWidth * 0.2) * Math.random() + hexWrap.offsetWidth * 0.3;
+}
+
+function getHeight() {
+  return (hexWrap.offsetHeight * 0.2) * Math.random() + hexWrap.offsetHeight * 0.3;
+}
+
+function lengthPx(fun) {
+	return fun() + 'px';
+}
+
+hexDye.style.left = lengthPx(getLeft);
+hexDye.style.top = lengthPx(getTop);
+hexDye.style.width = lengthPx(getWidth);
+hexDye.style.height = lengthPx(getWidth);
+
+setInterval(function() {
+  hexDye.style.left = lengthPx(getLeft);
+  hexDye.style.top = lengthPx(getTop);
+  hexDye.style.width = lengthPx(getWidth);
+  hexDye.style.height = lengthPx(getWidth);
+}, 10000);
