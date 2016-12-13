255b095ec97cfd7df1f4bddb1cad0c0719ca944a
feichao <len.may@foxmail.com>
2016-12-13 10:53:41 +0800

    保证每次获取的left和top都是顶点

--- index.js
+++ index.js
@@ -2,35 +2,66 @@ var hexWrap = document.getElementById('hex-wrap');
 var hexDye = document.getElementById('hex-dye');
 var transformOrigin = ['left top', 'right top', 'left bottom', 'right bottom', 'center center'];
 
+var PER_LEFT = 30 * 6;
+var LEFT_DYE_ODD_ARR = [30, 150];
+var LEFT_DYE_EVEN_ARR = [60, 120];
+
+var PER_TOP = 30 * 1.7320508;
+
+
 function getLeft() {
-  return hexWrap.offsetWidth / 2 * Math.random();
+  return Math.round((hexWrap.offsetWidth * Math.random() / 2) / PER_LEFT) * PER_LEFT;
 }
 
 function getTop() {
-  return hexWrap.offsetHeight / 2 * Math.random();
+  return Math.round((hexWrap.offsetHeight * Math.random() / 2) / PER_TOP) * PER_TOP;
 }
 
 function getWidth() {
-  return (hexWrap.offsetWidth * 0.6) * Math.random() + hexWrap.offsetWidth * 0.1;
+  return (hexWrap.offsetWidth * 0.2) * Math.random() + hexWrap.offsetWidth * 0.2;
 }
 
 function getHeight() {
-  return (hexWrap.offsetHeight * 0.6) * Math.random() + hexWrap.offsetHeight * 0.1;
+  return (hexWrap.offsetHeight * 0.2) * Math.random() + hexWrap.offsetHeight * 0.2;
 }
 
 function lengthPx(fun) {
-	return fun() + 'px';
+	if(typeof fun === 'function') {
+		return fun() + 'px';
+	}
+
+	return fun + 'px';
 }
 
-hexDye.style.left = lengthPx(getLeft);
-hexDye.style.top = lengthPx(getTop);
+function getPosition() {
+	var left;
+	var top = getTop();
+	if(top % PER_TOP < 0.05) {
+		left = getLeft() + LEFT_DYE_EVEN_ARR[Math.round(Math.random())];
+	} else {
+		left = getLeft() + LEFT_DYE_ODD_ARR[Math.round(Math.random())];
+	}
+
+	return {
+		left: left,
+		top: top
+	}
+}
+
+var position = getPosition();
+
+hexDye.style.left = lengthPx(position.left);
+hexDye.style.top = lengthPx(position.top);
 hexDye.style.width = lengthPx(getWidth);
 hexDye.style.height = lengthPx(getWidth);
 
 setInterval(function() {
-  hexDye.style.left = lengthPx(getLeft);
-  hexDye.style.top = lengthPx(getTop);
+  position = getPosition();
+
+	hexDye.style.left = lengthPx(position.left);
+	hexDye.style.top = lengthPx(position.top);
+
   hexDye.style.width = lengthPx(getWidth);
   hexDye.style.height = lengthPx(getWidth);
   hexDye.style.transformOrigin = transformOrigin[Math.floor(Math.random() * transformOrigin.length)];
-}, 5000);
+}, 10000);
